694821dbb72977b7bc42b5e2fb8f716c9b3523a4
Marcin Niedbalec <belkocik97@interia.pl>
2024-01-14 21:10:19 +0100

    feat: jwt strategy

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
-import { Logger } from 'nestjs-pino';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import { AuthModule } from './auth.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
