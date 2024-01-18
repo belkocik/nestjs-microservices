43dcc0cf94d17650ed9265fcd43112be49ab0adf
Marcin Niedbalec <belkocik97@interia.pl>
2024-01-18 21:57:12 +0100

    feat: payments microservice - stripe

---
+++ apps/payments/src/payments.controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateChargeDto } from '@app/common';
+
+@Controller()
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+  @MessagePattern('create-charge')
+  async createCharge(@Payload() data: CreateChargeDto) {
+    return this.paymentsService.createCharge(data);
+  }
+}
